a622848b42df16ed726c063f1f812728ea077bd8
karam <karamkaku2000@gmail.com>
2021-08-12 02:37:13 +0200

    add animation in Intersection

--- templates/title.js
+++ templates/title.js
@@ -1,5 +1,6 @@
-import styled from "styled-components";
+import styled,{css} from "styled-components";
 import {devices} from "../styles/media";
+import {fadeInUp} from "../styles/animation";
 const Bullet = styled.span`
  border-radius:50%;
  color:var(--blue);
@@ -10,7 +11,13 @@ const Title = styled.h2`
 color:var(--primary);
 font-size:3.4rem;
 text-transform:capitalize;
-
+transform :translateY(100%);
+opacity:0;
+${({isVisible})=>isVisible?`
+transition :opacity .5s  ease-out,transform .5s ease-out;
+opacity:1;  
+transform:translateY(0%); 
+ `:null}
 @media(${devices.small}){
  font-size:2.4rem;
 }
