251138e5602404bad9f532fd1b497c4db70e55cd
karam <karamkaku2000@gmail.com>
2021-08-14 02:50:26 +0200

    remove layout component

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,12 @@
-import Layout from '../components/common/layout'
+
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps}) {
   return(
-   <Layout>
      <Component {...pageProps} />
-   </Layout>
   )
 }
+
+
 
 export default MyApp
