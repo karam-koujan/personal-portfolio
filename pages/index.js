3487b999f0d32be750f1db4fdf4b6fa7a24136fc
karam <karamkaku2000@gmail.com>
2021-08-14 02:51:19 +0200

    get data from content directory

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,32 @@
-
+import {getContentByFolderName} from "../lib/queryMarkdown";
 import { About ,Contact , Introduction, Projects } from "../components/landing"
+import Layout from "../components/common/layout";
 
-export default function Home() {
+export default function Home({about,contact,projects:{projects},introduction,navigation:{navigation}}) {
+  
   return (
-     <>
-     <Introduction/>
-     <About/>
-     <Projects/>
-     <Contact/>
-    </>
+     <Layout data={navigation}>
+     <Introduction data={introduction}/>
+     <About data={about}/>
+     <Projects data={projects}/>
+     <Contact data={contact}/>
+    </Layout>
   )
+}
+
+export const getStaticProps = async()=>{
+  const introduction = getContentByFolderName("introduction");
+  const navigation = getContentByFolderName("navigation");
+  const about = getContentByFolderName("about");
+  const contact = getContentByFolderName("contact");
+  const projects = getContentByFolderName("projects")
+  return {
+    props : {
+      about:about.data,
+      contact:contact.data,
+      projects:projects.data,
+      introduction : introduction.data,
+      navigation : navigation.data
+    }
+  }
 }
