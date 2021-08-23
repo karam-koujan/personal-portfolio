a94512c58ce73064b7b957d03a388ef3cf749f06
karam <karamkaku2000@gmail.com>
2021-08-23 15:56:54 +0200

    add data folder

---
+++ data/config.js
@@ -0,0 +1,14 @@
+module.exports =  {
+    title:"karam koujan | Full-stack Dev",
+    discription:"I'm karam koujan a self-taught javascript developer , interested in the web & machine learning .",
+    socialMedia : {
+       twitter:"https://twitter/karam-kaku",
+       github: 'https://github.com/karam-koujan'
+    },
+    image:"/public/assets/images/profileImg.jpg",
+    url:"",
+    contact: {
+        email: 'karamkaku2000@gmail.com',
+        phone: '+212-691-06-60-69',
+      },
+}
