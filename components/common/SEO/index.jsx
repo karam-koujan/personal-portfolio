8a45245abace39cb173c38d4693e383be8496299
karam <karamkaku2000@gmail.com>
2021-08-23 17:41:55 +0200

    fix a typo

--- components/common/SEO/index.jsx
+++ components/common/SEO/index.jsx
@@ -1,15 +1,15 @@
 import Head from "next/head";
-import {title,discription,url,image,name} from "../../../data/config";
+import {title,description,url,image,name} from "../../../data/config";
 
 
 const SEO = ()=>{
   return(
       <Head>
            <title>{title}</title>
-           <meta charset="UTF-8"/>
+           <meta charSet="UTF-8"/>
            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
             <meta name="author" content={name}/>
-           <meta name="discription" content={discription}/>
+           <meta name="description" content={description}/>
            <meta name="robots" content="index, follow" />
            <meta name="og:type" content="website"/>
            <meta name="og:title" content={title}/>
@@ -17,7 +17,7 @@ const SEO = ()=>{
            <meta name="og:image" content={image}/>
            <meta name="og:image:alt" content="karam koujan's image"/>
            <meta name="og:site_name" content="personal portfolio"/>
-           <meta name="og:discription" content={discription}/>
+           <meta name="og:description" content={description}/>
            <meta name="twitter:card" content="summary"/>
            <meta name="twitter:type" content="website"/>
            <meta name="twitter:title" content={title}/>
