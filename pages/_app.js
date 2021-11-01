f83b8965e9e78321f48f35cea70f0ac0c2191abd
karam <karamkaku2000@gmail.com>
2021-11-01 01:31:18 +0100

    add themeProvider

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,10 @@ import { ThemeProvider } from 'styled-components';
 import {GlobalStyle} from "../styles/globalstyle";
 function MyApp({ Component, pageProps}) {
   return(
-    <>    
+    <ThemeProvider theme={{primary:"fffff"}}>    
    <GlobalStyle/>
    <Component {...pageProps} />   
-  </>
+  </ThemeProvider>
   )
 }
 
