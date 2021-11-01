646a86940e8e291e3534e56a21736724bec7d0b7
karam <karamkaku2000@gmail.com>
2021-11-01 01:40:12 +0100

    add serverside rendering

--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,33 @@
 import Document,{Main,Head,NextScript,Html} from "next/document";
+import { ServerStyleSheet } from 'styled-components'
 
 
 class MyDocument extends Document {
-    static async getInitialProps(ctx){
-        const initialProps = await Document.getInitialProps(ctx);
-        return {...initialProps}
-  }
+    static async getInitialProps(ctx) {
+        const sheet = new ServerStyleSheet()
+        const originalRenderPage = ctx.renderPage
+    
+        try {
+          ctx.renderPage = () =>
+            originalRenderPage({
+              enhanceApp: (App) => (props) =>
+                sheet.collectStyles(<App {...props} />),
+            })
+    
+          const initialProps = await Document.getInitialProps(ctx)
+          return {
+            ...initialProps,
+            styles: (
+              <>
+                {initialProps.styles}
+                {sheet.getStyleElement()}
+              </>
+            ),
+          }
+        } finally {
+          sheet.seal()
+        }
+      }
     render(){
         return(
             <Html lang="en">
