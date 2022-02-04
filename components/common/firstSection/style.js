95913f32236b4522ee8112a77df6d06afbcbc2d2
karam koujan <62403426+karam-koujan@users.noreply.github.com>
2022-02-04 22:42:35 +0100

    Update style.js

--- components/common/firstSection/style.js
+++ components/common/firstSection/style.js
@@ -4,26 +4,23 @@ import {devices} from "../../../styles/media"
 
 
 export const Section = styled.section`
- background-color:var(--primary);
- display:flex;
+background-color:var(--primary);
+display:flex;
  justify-content:center;
  padding-block:min(5rem,10vw);
  padding-top:0 2rem;
  position:relative;
  &::before{
-  background-color:transparent;
-  border-top:0px solid white;
-  border-left:1400px solid transparent;
-  border-bottom:70px solid white;
-  border-right:0px solid white;
+  background-color:white;
+  clip-path:polygon(100% 0%,0% 100%,100% 100%);
   bottom:-1px; 
-  content:"";   
-  height:40px;
+  content:"";
+  left:-40px;
+  right:0px;   
+  height:115px;
   position:absolute;
-  width:100%;
   }
   @media(${devices.small}){
     padding-left:0rem;
    }
 `
-
