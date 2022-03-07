cc20d9b139f6a623bf3c924109d6513a04d506e1
karam koujan <62403426+karam-koujan@users.noreply.github.com>
2022-03-07 18:25:08 +0100

    Update index.jsx

--- components/landing/introduction/index.jsx
+++ components/landing/introduction/index.jsx
@@ -12,7 +12,7 @@ const Introduction = ({data:{name,introduction}})=>{
             {introduction}
           </Text>
           <BtnWrapper>
-            <Btn tabIndex="0" href="https://drive.google.com/file/d/1SjfwVSkApvdLbiiaaWURRkKlE_3rcDZQ/view?usp=sharing" target="https://drive.google.com/file/d/1SjfwVSkApvdLbiiaaWURRkKlE_3rcDZQ/view?usp=sharing">
+            <Btn tabIndex="0" href="https://drive.google.com/file/d/1CHCDx-s_nTUyvBJ1oOFSbu0ON9YtSLn-/view?usp=sharing" target="https://drive.google.com/file/d/1CHCDx-s_nTUyvBJ1oOFSbu0ON9YtSLn-/view?usp=sharing">
               my resume
             </Btn>
           <Link href="/contact" >
